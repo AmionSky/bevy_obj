676628598b7c10ad3554ee089f3e8cce1911e8a8
Csányi István <csanyistvan02@gmail.com>
2020-08-18 13:42:05 +0200

    Fix

--- src/loader.rs
+++ src/loader.rs
@@ -26,55 +26,12 @@ impl AssetLoader<Mesh> for ObjLoader {
 pub enum ObjError {
     #[error("Invalid OBJ file.")]
     Gltf(#[from] obj::ObjError),
-    #[error("Unknown vertex format.")]
-    UnknownVertexFormat,
 }
 
 fn load_obj(bytes: Vec<u8>) -> Result<Mesh, ObjError> {
-    let raw = obj::raw::parse_obj(bytes.as_slice())?;
-
-    // Get the most complete vertex representation
-    //  3 => Position, Normal, Texture
-    //  2 => Position, Normal
-    //  1 => Position
-    let mut pnt = 3;
-    for polygon in &raw.polygons {
-        use obj::raw::object::Polygon;
-        match polygon {
-            Polygon::P(_) => pnt = std::cmp::min(pnt, 1),
-            Polygon::PN(_) => pnt = std::cmp::min(pnt, 2),
-            _ => {}
-        }
-    }
-
-    let mut mesh = Mesh::new(PrimitiveTopology::TriangleStrip);
-    match pnt {
-        1 => load_obj_p(obj::Obj::new(raw)?, &mut mesh),
-        2 => load_obj_pn(obj::Obj::new(raw)?, &mut mesh),
-        3 => load_obj_pnt(obj::Obj::new(raw)?, &mut mesh),
-        _ => return Err(ObjError::UnknownVertexFormat),
-    }
-
+    let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
+    load_obj_pnt(obj::load_obj(bytes.as_slice())?, &mut mesh);
     Ok(mesh)
-}
-
-fn load_obj_p(obj: obj::Obj<obj::Position>, mesh: &mut Mesh) {
-    mesh.attributes.push(VertexAttribute::position(
-        obj.vertices.iter().map(|v| v.position).collect(),
-    ));
-
-    set_mesh_indices(mesh, obj);
-}
-
-fn load_obj_pn(obj: obj::Obj<obj::Vertex>, mesh: &mut Mesh) {
-    mesh.attributes.push(VertexAttribute::position(
-        obj.vertices.iter().map(|v| v.position).collect(),
-    ));
-    mesh.attributes.push(VertexAttribute::normal(
-        obj.vertices.iter().map(|v| v.normal).collect(),
-    ));
-
-    set_mesh_indices(mesh, obj);
 }
 
 fn load_obj_pnt(obj: obj::Obj<obj::TexturedVertex>, mesh: &mut Mesh) {
@@ -87,7 +44,8 @@ fn load_obj_pnt(obj: obj::Obj<obj::TexturedVertex>, mesh: &mut Mesh) {
     mesh.attributes.push(VertexAttribute::uv(
         obj.vertices
             .iter()
-            .map(|v| [v.texture[0], v.texture[1]])
+            // Flip UV for correct values
+            .map(|v| [v.texture[0], 1.0 - v.texture[1]])
             .collect(),
     ));
 
