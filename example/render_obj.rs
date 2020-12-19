a6ff433f3477808c3087c36ef1284d9aabf1daac
Csányi István <csanyistvan02@gmail.com>
2020-12-19 21:12:08 +0100

    Upgraded to bevy 0.4

--- example/render_obj.rs
+++ example/render_obj.rs
@@ -11,24 +11,23 @@ fn main() {
 }
 
 fn setup(
-    mut commands: Commands,
+    commands: &mut Commands,
     asset_server: Res<AssetServer>,
-    mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
     commands
-        .spawn(PbrComponents {
+        .spawn(PbrBundle {
             mesh: asset_server
                 .load("cube.obj"),
             material: materials.add(Color::rgb(0.5, 0.4, 0.3).into()),
             ..Default::default()
         })
-        .spawn(LightComponents {
+        .spawn(LightBundle {
             transform: Transform::from_translation(Vec3::new(4.0, 5.0, 4.0)),
             ..Default::default()
         })
-        .spawn(Camera3dComponents {
-            transform: Transform::from_translation(Vec3::new(1.0, 2.7, 3.0))
+        .spawn(Camera3dBundle {
+            transform: Transform::from_translation(Vec3::new(1.5, 2.7, 3.0))
                 .looking_at(Vec3::new(0.0, 0.3, 0.0), Vec3::unit_y()),
             ..Default::default()
         });
