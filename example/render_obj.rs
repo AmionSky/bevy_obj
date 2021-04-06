8700c50d6817d15e5614719876566ea69623936e
Csányi István <csanyistvan02@gmail.com>
2021-04-06 23:33:11 +0200

    Update to 0.5

--- example/render_obj.rs
+++ example/render_obj.rs
@@ -3,7 +3,7 @@ use bevy_obj::*;
 
 fn main() {
     App::build()
-        .add_resource(Msaa { samples: 4 })
+        .insert_resource(Msaa { samples: 4 })
         .add_plugins(DefaultPlugins)
         .add_plugin(ObjPlugin)
         .add_startup_system(setup.system())
@@ -11,24 +11,22 @@ fn main() {
 }
 
 fn setup(
-    commands: &mut Commands,
+    mut commands: Commands,
     asset_server: Res<AssetServer>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    commands
-        .spawn(PbrBundle {
-            mesh: asset_server
-                .load("cube.obj"),
-            material: materials.add(Color::rgb(0.5, 0.4, 0.3).into()),
-            ..Default::default()
-        })
-        .spawn(LightBundle {
-            transform: Transform::from_translation(Vec3::new(4.0, 5.0, 4.0)),
-            ..Default::default()
-        })
-        .spawn(Camera3dBundle {
-            transform: Transform::from_translation(Vec3::new(1.5, 2.7, 3.0))
-                .looking_at(Vec3::new(0.0, 0.3, 0.0), Vec3::unit_y()),
-            ..Default::default()
-        });
+    commands.spawn_bundle(PbrBundle {
+        mesh: asset_server.load("cube.obj"),
+        material: materials.add(Color::rgb(0.5, 0.4, 0.3).into()),
+        ..Default::default()
+    });
+    commands.spawn_bundle(LightBundle {
+        transform: Transform::from_translation(Vec3::new(4.0, 5.0, 4.0)),
+        ..Default::default()
+    });
+    commands.spawn_bundle(PerspectiveCameraBundle {
+        transform: Transform::from_translation(Vec3::new(1.5, 2.7, 3.0))
+            .looking_at(Vec3::new(0.0, 0.3, 0.0), Vec3::Y),
+        ..Default::default()
+    });
 }
