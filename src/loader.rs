2f1c8e29d75f1588b824631d80ea3088df9d20f1
Csányi István <csanyistvan02@gmail.com>
2021-04-06 23:33:52 +0200

    Load objs without uv or normal

--- src/loader.rs
+++ src/loader.rs
@@ -29,6 +29,8 @@ impl AssetLoader for ObjLoader {
 pub enum ObjError {
     #[error("Invalid OBJ file.")]
     Gltf(#[from] obj::ObjError),
+    #[error("Unknown vertex format.")]
+    UnknownVertexFormat,
 }
 
 async fn load_obj<'a, 'b>(
@@ -36,28 +38,73 @@ async fn load_obj<'a, 'b>(
     load_context: &'a mut LoadContext<'b>,
 ) -> Result<(), ObjError> {
     let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
-    load_obj_pnt(obj::load_obj(bytes)?, &mut mesh);
+    load_obj_from_bytes(bytes, &mut mesh)?;
     load_context.set_default_asset(LoadedAsset::new(mesh));
     Ok(())
 }
 
-fn load_obj_pnt(obj: obj::Obj<obj::TexturedVertex>, mesh: &mut Mesh) {
-    let uvs = VertexAttributeValues::Float3(
-        obj.vertices
-            .iter()
-            // Flip UV for correct values
-            .map(|v| [v.texture[0], 1.0 - v.texture[1], v.texture[2]])
-            .collect(),
-    );
-    let positions =
-        VertexAttributeValues::Float3(obj.vertices.iter().map(|v| v.position).collect());
-    let normals = VertexAttributeValues::Float3(obj.vertices.iter().map(|v| v.normal).collect());
+fn load_obj_from_bytes(bytes: &[u8], mesh: &mut Mesh) -> Result<(), ObjError> {
+    let raw = obj::raw::parse_obj(bytes)?;
 
+    // Get the most complete vertex representation
+    //  3 => Position, Normal, Texture
+    //  2 => Position, Normal
+    //  1 => Position
+    let mut pnt = 3;
+    for polygon in &raw.polygons {
+        use obj::raw::object::Polygon;
+        match polygon {
+            Polygon::P(_) => pnt = std::cmp::min(pnt, 1),
+            Polygon::PN(_) => pnt = std::cmp::min(pnt, 2),
+            _ => {}
+        }
+    }
+
+    match pnt {
+        1 => {
+            let obj: obj::Obj<obj::Position> = obj::Obj::new(raw)?;
+            set_position_data(mesh, obj.vertices.iter().map(|v| v.position).collect());
+            set_mesh_indices(mesh, obj);
+        }
+        2 => {
+            let obj: obj::Obj<obj::Vertex> = obj::Obj::new(raw)?;
+            set_position_data(mesh, obj.vertices.iter().map(|v| v.position).collect());
+            set_normal_data(mesh, obj.vertices.iter().map(|v| v.normal).collect());
+            set_mesh_indices(mesh, obj);
+        }
+        3 => {
+            let obj: obj::Obj<obj::TexturedVertex> = obj::Obj::new(raw)?;
+            set_position_data(mesh, obj.vertices.iter().map(|v| v.position).collect());
+            set_normal_data(mesh, obj.vertices.iter().map(|v| v.normal).collect());
+            set_uv_data(
+                mesh,
+                obj.vertices
+                    .iter()
+                    // Flip UV for correct values
+                    .map(|v| [v.texture[0], 1.0 - v.texture[1], v.texture[2]])
+                    .collect(),
+            );
+            set_mesh_indices(mesh, obj);
+        }
+        _ => return Err(ObjError::UnknownVertexFormat),
+    }
+
+    Ok(())
+}
+
+fn set_position_data(mesh: &mut Mesh, data: Vec<[f32; 3]>) {
+    let positions = VertexAttributeValues::Float3(data);
     mesh.set_attribute(Mesh::ATTRIBUTE_POSITION, positions);
-    mesh.set_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
-    mesh.set_attribute(Mesh::ATTRIBUTE_UV_0, uvs);
+}
 
-    set_mesh_indices(mesh, obj);
+fn set_normal_data(mesh: &mut Mesh, data: Vec<[f32; 3]>) {
+    let normals = VertexAttributeValues::Float3(data);
+    mesh.set_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
+}
+
+fn set_uv_data(mesh: &mut Mesh, data: Vec<[f32; 3]>) {
+    let uvs = VertexAttributeValues::Float3(data);
+    mesh.set_attribute(Mesh::ATTRIBUTE_UV_0, uvs);
 }
 
 fn set_mesh_indices<T>(mesh: &mut Mesh, obj: obj::Obj<T>) {
