c2983e80efbcfce336ac41cb68aaccb60f9e6520
Luca Della Vedova <lucadv@intrinsic.ai>
2023-04-12 12:11:15 +0800

    Merge meshes to avoid breaking behavior
    Signed-off-by: Luca Della Vedova <lucadv@intrinsic.ai>

--- src/loader.rs
+++ src/loader.rs
@@ -29,8 +29,6 @@ impl AssetLoader for ObjLoader {
 pub enum ObjError {
     #[error("Invalid OBJ file: {0}")]
     TobjError(#[from] tobj::LoadError),
-    #[error("Unexpected number of meshes, only 1 is supported")]
-    WrongMeshNumber,
 }
 
 async fn load_obj<'a, 'b>(
@@ -43,52 +41,59 @@ async fn load_obj<'a, 'b>(
 }
 
 fn load_mtl(_path: &std::path::Path) -> tobj::MTLLoadResult {
-    //TODO(luca) Implement mtl loading
     Err(tobj::LoadError::OpenFileFailed)
 }
 
 pub fn load_obj_from_bytes(mut bytes: &[u8]) -> Result<Mesh, ObjError> {
     let options = tobj::GPU_LOAD_OPTIONS;
-    match tobj::load_obj_buf(&mut bytes, &options, load_mtl) {
-        Ok(obj) => {
-            if obj.0.len() != 1 {
-                return Err(ObjError::WrongMeshNumber);
-            }
-            let mesh = &obj.0[0].mesh;
-
-            let vertex_position: Vec<[f32; 3]> = mesh
+    let obj = tobj::load_obj_buf(&mut bytes, &options, load_mtl)?;
+    let mut indices = Vec::new();
+    let mut vertex_position = Vec::new();
+    let mut vertex_normal = Vec::new();
+    let mut vertex_texture = Vec::new();
+    for model in obj.0 {
+        indices.reserve(model.mesh.indices.len());
+        vertex_position.reserve(model.mesh.positions.len() / 3);
+        vertex_normal.reserve(model.mesh.normals.len() / 3);
+        vertex_texture.reserve(model.mesh.texcoords.len() / 2);
+        vertex_position.extend(
+            model
+                .mesh
                 .positions
                 .chunks_exact(3)
-                .map(|v| [v[0], v[1], v[2]])
-                .collect();
-            let vertex_normal: Vec<[f32; 3]> = mesh
+                .map(|v| [v[0], v[1], v[2]]),
+        );
+        vertex_normal.extend(
+            model
+                .mesh
                 .normals
                 .chunks_exact(3)
-                .map(|n| [n[0], n[1], n[2]])
-                .collect();
-            let vertex_texture: Vec<[f32; 2]> = mesh
+                .map(|n| [n[0], n[1], n[2]]),
+        );
+        vertex_texture.extend(
+            model
+                .mesh
                 .texcoords
                 .chunks_exact(2)
-                .map(|t| [t[0], 1.0 - t[1]])
-                .collect();
-
-            let mut bevy_mesh = Mesh::new(PrimitiveTopology::TriangleList);
-
-            bevy_mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, vertex_position);
-            if !vertex_texture.is_empty() {
-                bevy_mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, vertex_texture);
-            }
-
-            if !vertex_normal.is_empty() {
-                bevy_mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, vertex_normal);
-            } else {
-                bevy_mesh.compute_flat_normals();
-            }
-
-            bevy_mesh.set_indices(Some(Indices::U32(mesh.indices.clone())));
-
-            Ok(bevy_mesh)
-        }
-        Err(err) => Err(ObjError::TobjError(err)),
+                .map(|t| [t[0], 1.0 - t[1]]),
+        );
+        indices.extend(model.mesh.indices);
     }
+
+    let mut bevy_mesh = Mesh::new(PrimitiveTopology::TriangleList);
+
+    bevy_mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, vertex_position);
+    if !vertex_texture.is_empty() {
+        bevy_mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, vertex_texture);
+    }
+
+    if !vertex_normal.is_empty() {
+        bevy_mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, vertex_normal);
+    } else {
+        bevy_mesh.compute_flat_normals();
+    }
+
+    bevy_mesh.set_indices(Some(Indices::U32(indices)));
+
+    Ok(bevy_mesh)
 }
